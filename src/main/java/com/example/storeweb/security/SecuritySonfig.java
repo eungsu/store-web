d07aecd1dfc4cede41fcd52c8452def56cf73c79
eungsu <eungsu@me.com>
2022-02-19 23:23:56 +0900

    북스토어 웹 프로젝트 생성

---
+++ src/main/java/com/example/storeweb/security/SecuritySonfig.java
@@ -0,0 +1,23 @@
+package com.example.storeweb.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecuritySonfig extends WebSecurityConfigurerAdapter {
+
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
+       http
+        .authorizeRequests()
+        .antMatchers("/").permitAll()
+        .anyRequest().authenticated();
+   }
+
+   @Override
+   public void configure(WebSecurity web) throws Exception {
+       web.ignoring().antMatchers("/h2-console/**");
+   }
+}
