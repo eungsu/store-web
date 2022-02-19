f6a6552f9b9fb9994a3aaa08a1b68c5de971d584
eungsu <eungsu@me.com>
2022-02-19 23:25:43 +0900

    엔티티, 레파지토리 클래스 정의

---
+++ src/main/java/com/example/storeweb/entity/Member.java
@@ -0,0 +1,37 @@
+package com.example.storeweb.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "member_email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "member_password", nullable = false)
+    private String password;
+
+    @Column(name = "member_name", nullable = false)
+    private String name;
+
+    @Column(name = "member_tel")
+    private String tel;
+
+    @Column(name = "member_enabled")
+    private boolean enabled;
+}
