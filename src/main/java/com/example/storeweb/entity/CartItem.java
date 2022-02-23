3d6646391a3312f0d3771fffaba1f95458e86e0a
eungsu <eungsu@me.com>
2022-02-23 12:55:56 +0900

    CartItem의 @ManyToOne에 optional, fetch 속성 추가

--- src/main/java/com/example/storeweb/entity/CartItem.java
+++ src/main/java/com/example/storeweb/entity/CartItem.java
@@ -2,6 +2,7 @@ package com.example.storeweb.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,12 +23,12 @@ public class CartItem extends BaseTimeEntity {
     @Column(name = "cart_item_id")
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "book_id", nullable = false)
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "book_id")
     private Book book;
     
-    @ManyToOne
-    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
     
     @Column(name = "cart_item_quantity")
